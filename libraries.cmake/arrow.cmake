d80563577d51f2d6fff959fecdc9f4bb901dd9d6
Patron <Patron@Patrons-MacBook-Air.local>
2025-06-21 19:10:17 -0500

    can't find OPENMS_BUILD_...ARROW?

--- libraries.cmake/arrow.cmake
+++ libraries.cmake/arrow.cmake
@@ -1,6 +1,7 @@
 #When the project starts, set up an isolated environment where you hardcode all these variables.
 # I want to see if I can exchange some parameters for others,
 MACRO(OPENMS_CONTRIB_BUILD_ARROW)
+    message(STATUS "does this even work??")
     OPENMS_LOGHEADER_LIBRARY("ARROW") 
     if(MSVC)
       set(ZIP_ARGS "x -y -osrc")
@@ -8,34 +9,65 @@ MACRO(OPENMS_CONTRIB_BUILD_ARROW)
       set(ZIP_ARGS "xzf")
     endif()
     OPENMS_SMARTEXTRACT(ZIP_ARGS ARCHIVE_ARROW "ARROW" "CREDITS")
-
+    if(UNIX AND NOT APPLE)
+      set(LINUX TRUE) #make a linux flag for readability.
+  endif()
 
     set( ARROW_BUILD_TYPE "static")
     if (BUILD_SHARED_LIBRARIES)
 	  set( ARROW_BUILD_TYPE "shared")
     endif()
-    
-    
- 
-  cmake_minimum_required(VERSION 3.16)
-  #I wonder if all cmake generators work with arrow? IDE vs Ninja for example? #TODO This is worth further testing.
-  #Also check in docs what users are required to install beforehand.
-#   if(Win32)
-#   execute_process(COMMAND ${CMAKE_COMMAND}
+    ##################################################################################
+if(APPLE) #both linux and apple fyi...
+  message(STATUS "Configuring Apache Arrow on macOS...")
 
-#   -D CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-#   -D CMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-#   -D BUILD_SHARED_LIBS=${BUILD_SHARED_LIBRARIES}
-#   -D CMAKE_DISABLE_FIND_PACKAGE_ICU=TRUE #https://cmake.org/cmake/help/latest/module/FindICU.html. 
-# # What do we use instead?
-#   -G "${CMAKE_GENERATOR}"
-#   ${ARCHITECTURE_OPTION_CMAKE}
-#   -D CMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}
-#   -D CMAKE_POSITION_INDEPENDENT_CODE=ON
-#   .
-#   WORKING_DIRECTORY ${ARROW_DIR}
-#   )
-	
+  set(_ARROW_CMAKE_ARGS
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+    -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBRARIES}
+    -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}
+    -DARROW_BUILD_TESTS=ON
+    -DARROW_BUILD_SHARED=${BUILD_SHARED_LIBRARIES}
+  )
+  message(STATUS "Setting working directory/output vars for args")
+  execute_process(COMMAND ${CMAKE_COMMAND}
+    ${_ARROW_CMAKE_ARGS}
+    .
+    WORKING_DIRECTORY ${ARROW_DIR}
+    OUTPUT_VARIABLE ARROW_CMAKE_OUT
+    ERROR_VARIABLE ARROW_CMAKE_ERR
+    RESULT_VARIABLE ARROW_CMAKE_SUCCESS
+  )
+
+  file(APPEND ${LOGFILE} ${ARROW_CMAKE_OUT} ${ARROW_CMAKE_ERR})
+
+  message(STATUS "Building and installing Apache Arrow on macOS...")
+
+  message(STATUS "")
+  execute_process(COMMAND ${CMAKE_COMMAND}
+    --build .
+    --config Release
+    --target install
+    WORKING_DIRECTORY ${ARROW_DIR}
+    OUTPUT_VARIABLE ARROW_BUILD_OUT
+    ERROR_VARIABLE ARROW_BUILD_ERR
+    RESULT_VARIABLE ARROW_BUILD_SUCCESS
+  )
+
+  file(APPEND ${LOGFILE} ${ARROW_BUILD_OUT} ${ARROW_BUILD_ERR})
+endif()
+  
+#############################################################################
+
+#BACK TO WINDOWS 
+  message(STATUS "Setting working directory/output vars for args")
+  execute_process(COMMAND ${CMAKE_COMMAND}
+    ${BUILD_SHARED_LIBS}
+        .
+    WORKING_DIRECTORY ${ARROW_DIR}
+    OUTPUT_VARIABLE ARROW_CMAKE_OUT
+    ERROR_VARIABLE ARROW_CMAKE_ERR
+    RESULT_VARIABLE ARROW_CMAKE_SUCCESS)
   execute_process(COMMAND ${CMAKE_COMMAND}
   --build .
   --config Release
